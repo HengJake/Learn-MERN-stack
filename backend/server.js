cf2865f0c40317ae501ea9a50da2fa96fc9419f0
Heng Jun Kai <hengjunkai@gmail.com>
2025-05-26 23:21:17 +0800

    add and delete item from collection in mongoDB

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); //allow json data in req.body
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in create Product : ", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+// dynamic it will get the id
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Product Not Found" });
+  }
+});
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server started at http://localhost:5000");
+});
